2a8eb21911fcac4d4f072dc6f73539bc41a4abc6
hilariojeda <josehilariojeda@gmail.com>
2023-10-02 20:48:06 -0600

    Crear proyecto

---
+++ inventariosystem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace inventariosystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
